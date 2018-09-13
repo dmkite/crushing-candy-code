b3609d57d5bf76b06e62d295ab09dce2b92d86bb
Dylan <kite.d92@gmail.com>
2018-09-13 09:21:50 -0700

    added functions for second object

--- stretch-exercises/index.js
+++ stretch-exercises/index.js
@@ -19,7 +19,7 @@ function createObj1_2(store1){
         let day = store1[keys]
         result[keys] = {}
         for(let i = 0; i < day.length; i++){
-            result[keys][store1[keys][i][0]] = store1[keys][i][1] * store1[keys][i][2]
+            result[keys][store1[keys][i][0]] = (store1[keys][i][1] * store1[keys][i][2]).toFixed(2)
         }
         
     }
@@ -38,21 +38,44 @@ function createObj2(store2){
     return result
 }
 
+// console.log(store2)
 function createObj2_2(store2){
-    let result = {}
-    for (let keys in store2['sale dates']){
-          let dates = store2['sale dates'][keys]
-          for (let i = 0; i < dates.length; i++){
-            if (!result[dates[i]]){
-                result[dates[i]] = {} 
-            }
-            else if(!result[dates[i]][keys]){
-              result[dates[i]][keys] = (dates.length * store2['inventory prices'][keys]).toFixed(2) 
-            }
+  let result2_2 = {}
+  for(let keys in store2['sale dates']){
+    //keys = candy name
+    //store2['sale dates'][keys] = array of dates
+    let dateArray = store2['sale dates'][keys]
+    for(let i = 0; i < dateArray.length; i++){
+      if(!result2_2[dateArray[i]]){
+        result2_2[dateArray[i]] = {}
+      }
+      if(!result2_2[dateArray[i]][keys]){
+           result2_2[dateArray[i]][keys] = Number((store2['inventory prices'][keys]).toFixed(2))
         }
-    }
-    return result
+      else{
+        result2_2[dateArray[i]][keys] += Number((store2['inventory prices'][keys]).toFixed(2))
+      }
+    }  
+    // console.log(result2_2)
+  }
+  return result2_2
 }
+// createObj2_2(store2)
+// function createObj2_2(store2){
+//     let result = {}
+//     for (let keys in store2['sale dates']){
+//           let dates = store2['sale dates'][keys]
+//           for (let i = 0; i < dates.length; i++){
+//             if (!result[dates[i]]){
+//                 result[dates[i]] = {} 
+//             }
+//             else if(!result[dates[i]][keys]){
+//               result[dates[i]][keys] = (dates.length * store2['inventory prices'][keys]).toFixed(2) 
+//             }
+//         }
+//     }
+//     return result
+// }
 
 
 function createObj3(store3){
@@ -61,7 +84,6 @@ function createObj3(store3){
         let day = store3[i]
         for (let keys in day['inventory sold']){
             let candy = keys
-            console.log(candy)
             if(!result[candy]){
                 result[candy] = {}
                 result[candy].cost = day['inventory sold'][keys].cost
@@ -76,18 +98,34 @@ function createObj3(store3){
 }
 
 function createObj3_2(store3){
-    let result = {}
+    let result3_2 = {}
     for(let i = 0; i < store3.length; i++){
         let info = store3[i]
-        if(!result[info.date]){
-            result[info.date] = {}
+        if(!result3_2[info.date]){
+            result3_2[info.date] = {}
         }
         for(let keys in info['inventory sold']){
-            result[info.date][keys] = (info['inventory sold'][keys].cost * info['inventory sold'][keys].quantity).toFixed(2)
+            result3_2[info.date][keys] = (info['inventory sold'][keys].cost * info['inventory sold'][keys].quantity).toFixed(2)
         }
     }
 
-    return result
+    return result3_2
+}
+
+    
+    function createObj3_2(store3){
+    let result3_2 = {}
+    for(let i = 0; i < store3.length; i++){
+        let info = store3[i]
+        if(!result3_2[info.date]){
+            result3_2[info.date] = {}
+        }
+        for(let keys in info['inventory sold']){
+            result3_2[info.date][keys] = (info['inventory sold'][keys].cost * info['inventory sold'][keys].quantity).toFixed(2)
+        }
+    }
+
+    return result3_2
 }
 
 function createObj4(store4) {
@@ -99,17 +137,43 @@ function createObj4(store4) {
   }
   return result
 }
+console.log(store4)
 
 function createObj4_2(store4){
-    let result = {}
-    for (let keys in store4){
-      console.log(store4[keys]['sold on'])
-        for(let i = 0; i < store4[keys]['sold on'].length; i++){
-            if(!result[store4[keys]['sold on'][i]]){
-                result[store4[keys]['sold on'][i]] = {}
-            }
-            result[store4[keys]['sold on'][i]][keys] = (store4[keys]['sold on'].length * store4[keys].cost).toFixed(2)
+    let result4_2 = {}
+    for (let candies in store4){
+      let costSold = store4[candies]
+      for(let i = 0; i < costSold['sold on'].length; i++){
+        let date = costSold['sold on'][i]
+        if(!result4_2[date]){
+          result4_2[date] = {}
         }
+      }
     }
-    return result
+
+    for (let dates in result4_2){
+      for(let candies in store4){
+        result4_2[dates][candies] = 0
+      }
+    }
+
+    for( let stoCandies in store4){
+      let soldArr = store4[stoCandies]['sold on']
+      for(let i = 0; i < soldArr.length; i++){
+        let soldDate = soldArr[i]
+        for(let dates in result4_2){
+          for(let resCandies in result4_2[dates]){
+            if(stoCandies === resCandies && dates === soldDate){
+              result4_2[dates][resCandies] += store4[stoCandies]['cost']
+            }
+          }
+        }
+      }
+    }
+    return result4_2
 }
+
+
+
+
+
